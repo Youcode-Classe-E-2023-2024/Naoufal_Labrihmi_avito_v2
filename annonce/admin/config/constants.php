8e56ae06d3528e54a72aa4e4edfa998c9bc3f141
Naoufal <144787284+NaoufalLabrihmi@users.noreply.github.com>
2023-11-29 02:40:41 +0100

    Delete annonce directory

--- annonce/admin/config/constants.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-define('ROOT_URL', 'http://localhost/annonce/');
-define('DB_HOST', 'localhost');
-define('DB_USER', 'Naoufal');
-define('DB_PASS', '0207');
-define('DB_NAME', 'annonce');
--- annonce/js/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var userMenuButton = document.getElementById('user-menu-button');
-    var userDropdown = document.getElementById('user-dropdown');
-
-    userMenuButton.addEventListener('click', function () {
-        userDropdown.classList.toggle('hidden');
-    });
-});
